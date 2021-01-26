8e95ebee94fcb0df9649d8b549bef478fd1b5cf3
JeewanthaLahiru <jeewanthalahiru00@gmail.com>
2021-01-26 22:38:33 +0530

    added events to index.html

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component{
+class App extends Component{
   render(){
     return (
-      <div className="shopping-list">
-        <h1>Shopping List for {this.props.name}</h1>
-        <ul>
-          <li>Instagram</li>
-          <li>Whatsapp</li>
-          <li>Oculus</li>
-        </ul>
+      <div>
+        <h1>Hello world</h1>
       </div>
     );
   }
 }
-
 
